7a3e376c5bd36ff3b35e8e629103aa5967263330
Edouard de Laage <edouarddelaage@gmail.com>
2018-07-20 13:34:22 +0200

    Improuve routes for filde tel in the form footer

--- routes/auth/auth.js
+++ routes/auth/auth.js
@@ -10,48 +10,50 @@ Router.post('/send-email', function(req, res) {
   const last_name = req.body.last_name
   const email = req.body.email
   const messages = req.body.messages
+  const tel = req.body.tel
 
   const smtpTransport = nodemailer.createTestAccount((err, account) => {
     const transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
       port: 465,
-      secure: true, // true for 465, false for other ports
+      secure: true,
       auth: {
-          user: process.env.GMAIL_USER, // generated ethereal user
-          pass: process.env.GMAIL_PASSWORD // generated ethereal password
+          user: process.env.GMAIL_USER,
+          pass: process.env.GMAIL_PASSWORD
       }
     })
     const mailOptions = {
-      from: '"Edouard" <edouarddelaage@gmail.com>', // sender address
-      to: "edouarddelaage@gmail.com", // list of receivers
-      subject: 'Mesaage ', // Subject line
+      from: '"Edouard" <edouarddelaage@gmail.com>',
+      to: "edouarddelaage@gmail.com",
+      subject: 'Mesaage ',
       text: `Prenom : ${firts_name}
       Nom : ${last_name}
       email : ${email}
-      Message : ${messages}` // plaintext body
+      Message : ${messages}
+      Tel : ${tel}`
     };
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
           return console.log(error);
       }
       console.log('Message sent: %s', info.messageId);
-      // Preview only available when sending through an Ethereal account
+
       console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
       })
     })
 });
 
 const insertUserQuery = `
-  INSERT INTO messages (firts_name, last_name, email, messages)
-  VALUES (?, ?, ?, ?)`
+  INSERT INTO messages (firts_name, last_name, email, messages, tel)
+  VALUES (?, ?, ?, ?, ?)`
 
 Router.post('/messages', function(req, res, next) {
-  // res.send('I am in POST signup');
   const firts_name = req.body.firts_name
   const last_name = req.body.last_name
   const email = req.body.email
   const messages = req.body.messages
-  const values = [firts_name, last_name, email, messages]
+  const tel = req.body.tel
+  const values = [firts_name, last_name, email, messages, tel]
 
   connection.query(insertUserQuery, values)
     .then(result => {
